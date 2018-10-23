c363f11a7241808e3f80072667b138d7dc5d05f9
Diego Andrade de Mendonça <diego182@gmail.com>
2018-10-22 23:08:20 -0300

    Bootstrap files

---
+++ public/index.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+ * Require constants file
+ */
+require_once '../config/constants.php';
+
+/*
+ * Require vendor for composer.json
+ */
+require_once VENDOR_DIR . '/autoload.php';
+require_once CONFIG_DIR . '/bootstrap.php';
