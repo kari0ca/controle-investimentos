e6495c5cd691831e26424fd187b18da7af0d6926
Diego Andrade de Mendonça <diego182@gmail.com>
2018-10-22 23:06:01 -0300

    Moved some files arround

--- src/cadastr-usuario.php
+++ src/cadastr-usuario.php
@@ -5,42 +5,55 @@
    
    $error="";
    if($_SERVER["REQUEST_METHOD"] == "POST") {
-      // username and password sent from form 
+      $myusername = mysqli_real_escape_string($db,$_POST['nome']);
+      $mylogin = mysqli_real_escape_string($db,$_POST['username']);
+      $myemail = mysqli_real_escape_string($db,$_POST['email']);
+      $mypassword1 = mysqli_real_escape_string($db,$_POST['password1']);
+      $mypassword2 = mysqli_real_escape_string($db,$_POST['password2']); 
+      $mylembr = mysqli_real_escape_string($db,$_POST['lembrete']); 
       
-      $myusername = mysqli_real_escape_string($db,$_POST['username']);
-      $mypassword = mysqli_real_escape_string($db,$_POST['password']);
-      
-      $param_password = password_hash($mypassword, PASSWORD_DEFAULT);
-      echo "<br> pass orig=".$mypassword." pass_encript=".$param_password.",";
-      
-      $sql = "SELECT iduser, pass FROM investdb.user WHERE login = '$myusername'";
+      // Procura por usuarios com o mesmo login
+      $sql = "SELECT iduser FROM investdb.user WHERE login = '$mylogin'";
       $result = mysqli_query($db,$sql);
       $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-      $senha_hash = $row[pass];
       $active = $row['active'];
-      $iduser = $row[iduser];
-      
-      //compara senha com hash
-      if (password_verify ($mypassword, $senha_hash)){
-        echo "<br>Senha verificada com sucesso";
-      } else {
-        echo "<br>Senha não verificada";
-      }
       
       $count = mysqli_num_rows($result);
-      
-      // If result matched $myusername and $mypassword, table row must be 1 row
-		
-      if($count == 1) {
-         $_SESSION["login_user"] = $myusername;
-         $_SESSION["iduser"] = $iduser;
-         //session_register("myusername");
-         //$_SESSION['login_user'] = $myusername;
-         
-         header("location:carteira.php"); die('Não ignore meu cabeçalho...');
-      }else {
-         $error = "Login ou Senha invalidos";
+      echo "<br> count=".$count;
+      if($count >= 1) {
+        $error="Já existe um usuário com este nome, ";
+        echo "<br> Usuário existente";
       }
+      if($mypassword1!=$mypassword2) {
+        $error.="A confirmação da senha não é igual, ";
+      }
+      //remover 2 ultimos caracteres da string de erro
+      
+      
+      
+      echo "<br> antes de buscar o maior id";
+      // Obtem o maior id_user
+      $sql = "SELECT max(iduser) as iduser FROM investdb.user";
+      $result = mysqli_query($db,$sql) or die(mysql_error());
+      $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+      $iduser = $row[iduser];
+      $param_password = password_hash($mypassword1, PASSWORD_DEFAULT);
+      echo "<br> Maior ID=".$iduser;
+      $iduser = $iduser + 1;
+      echo "<br> Novo ID=".$iduser;
+      
+      echo "<br>Final das validações";
+      echo "<br>Erro=".$error;
+      echo "<br>dados de validação: Nome:".$myusername." Login:".$mylogin." Email:".$myemail." Password:".$param_password." Lembrete:".$mylembr;
+      $sql_insert = "INSERT INTO investdb.user values('".$iduser."','".$myusername."','".$mylogin."','".$param_password."','".$mylembr."','".$myemail."')";
+      echo "<br>".$sql_insert;
+      
+      if (mysqli_query($db, $sql_insert)) {
+        echo "New record created successfully";
+      } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+      }
+
    }
 ?>
 
@@ -101,12 +114,12 @@
     </div>
     <div class="collapse navbar-collapse" id="myNavbar">
       <ul class="nav navbar-nav">
-        <li class="active"><a href="./index.php">Início</a></li>
+        <li class="active"><a href="index.php">Início</a></li>
         <li><a href="./sobre.php">Sobre</a></li>
         <li><a href="./contato.php">Contato</a></li>
       </ul>
       <ul class="nav navbar-nav navbar-right">
-        <li><a href="./login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+        <li><a href="login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
       </ul>
     </div>
   </div>
@@ -125,17 +138,36 @@
 			<form action = "" method = "post">
         <div class="row">
           <div class="col-sm-4 form-group">
+            <input class="form-control" id="nome" name="nome" placeholder="Nome" type="text" required>
+          </div>
+          <div class="col-sm-4 form-group">
             <input class="form-control" id="login" name="username" placeholder="Login" type="text" required>
           </div>
-          <div class="col-sm-4 form-group">
-            <input class="form-control" id="pass" name="password" placeholder="Senha" type="password" required>
-          </div>
-
         </div>
         <div class="row">
-          <div class="col-sm-4 form-group text-align:center"><a href="cadastr-usuario.php">Cadastrar usuário</a></div>
+          <div class="col-sm-8 form-group">
+            <input class="form-control" id="email" name="email" placeholder="E-mail" type="email" required>
+          </div>
+        </div>
+        <div class="row">
           <div class="col-sm-4 form-group">
-            <button class="btn pull-right align:right" type="submit">Entrar</button>
+            <input class="form-control" id="pass1" name="password1" placeholder="Senha" type="password" required>
+          </div>
+          <div class="col-sm-4 form-group">
+            <input class="form-control" id="pass2" name="password2" placeholder="Repita a Senha" type="password" required>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-sm-4 form-group">
+            <input class="form-control" id="lembrete" name="lembrete" placeholder="Lembrete da senha" type="text" required>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-sm-4 form-group">
+            <button class="btn btn-danger pull-right align:right" type="reset">Cancelar</button>
+          </div>
+          <div class="col-sm-4 form-group">
+            <button class="btn btn-default pull-right align:left " type="submit">Cadastrar</button>
           </div>
         </div>
       </form>
--- src/login.php
+++ src/login.php
@@ -1,8 +1,53 @@
 <!DOCTYPE html>
+<?php
+   include("config.php");
+   session_start();
+   
+   $error="";
+   if($_SERVER["REQUEST_METHOD"] == "POST") {
+      // username and password sent from form 
+      
+      $myusername = mysqli_real_escape_string($db,$_POST['username']);
+      $mypassword = mysqli_real_escape_string($db,$_POST['password']);
+      
+      $param_password = password_hash($mypassword, PASSWORD_DEFAULT);
+      echo "<br> pass orig=".$mypassword." pass_encript=".$param_password.",";
+      
+      $sql = "SELECT iduser, pass FROM investdb.user WHERE login = '$myusername'";
+      $result = mysqli_query($db,$sql);
+      $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+      $senha_hash = $row[pass];
+      $active = $row['active'];
+      $iduser = $row[iduser];
+      
+      //compara senha com hash
+      if (password_verify ($mypassword, $senha_hash)){
+        echo "<br>Senha verificada com sucesso";
+      } else {
+        echo "<br>Senha não verificada";
+      }
+      
+      $count = mysqli_num_rows($result);
+      
+      // If result matched $myusername and $mypassword, table row must be 1 row
+		
+      if($count == 1) {
+         $_SESSION["login_user"] = $myusername;
+         $_SESSION["iduser"] = $iduser;
+         //session_register("myusername");
+         //$_SESSION['login_user'] = $myusername;
+         
+         header("location:carteira.php"); die('Não ignore meu cabeçalho...');
+      }else {
+         $error = "Login ou Senha invalidos";
+      }
+   }
+?>
+
 <html lang="en">
 <head>
   <title>Controle de investimentos</title>
-  <meta charset="utf-8">
+  <meta http-equiv="Content-Type" content="text/html;charset=iso-8859-1" />
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
@@ -43,8 +88,7 @@
 </head>
 <body>
 
-<!-- Barra de navegação --> 
-<!-- Depois trocar pela chamada da função que mostra a barra--> 
+<!-- Barra de navegação -->
 <nav class="navbar navbar-inverse">
   <div class="container-fluid">
     <div class="navbar-header">
@@ -57,69 +101,62 @@
     </div>
     <div class="collapse navbar-collapse" id="myNavbar">
       <ul class="nav navbar-nav">
-        <li class="active"><a href="./index.php">Início</a></li>
+        <li class="active"><a href="index.php">Início</a></li>
         <li><a href="./sobre.php">Sobre</a></li>
         <li><a href="./contato.php">Contato</a></li>
       </ul>
       <ul class="nav navbar-nav navbar-right">
-        <li><a href="./login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+        <li><a href="login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
       </ul>
     </div>
   </div>
 </nav>
-
-
     
 
 <!-- Conteúdo -->
-<div id="myCarousel" class="carousel slide" data-ride="carousel">
-  <!-- Indicators -->
-  <ol class="carousel-indicators">
-    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-    <li data-target="#myCarousel" data-slide-to="1"></li>
-    <li data-target="#myCarousel" data-slide-to="2"></li>
-  </ol>
+<p></p>
+<span></span>
+<span></span>
 
-  <!-- Wrapper for slides -->
-  <div class="carousel-inner" role="listbox">
-    <div class="item active">
-      <img src="./images/porq.jpg" alt="New York">
-      <div class="carousel-caption">
-        <h1>Gerencie seus investimentos</h1>
-        <h3>Use a nossa plataforma para controlar melhor os seus investimentos</h3>
-      </div> 
-    </div>
 
-    <div class="item">
-      <img src="./images/invest.jpg" alt="Chicago">
-      <div class="carousel-caption">
-        <h1>Acompanhe o seu investimento</h1>
-        <h3>Saiba de forma simples e direta o rendimento e performance dos seus investimentos</h3>
-      </div> 
-    </div>
+<div class="container">
+	<div class="row justify-content-center">
+		<div class="col-md-12">
+			<form action = "" method = "post">
+        <div class="row">
+          <div class="col-sm-4 form-group">
+            <input class="form-control" id="login" name="username" placeholder="Login" type="text" required>
+          </div>
+          <div class="col-sm-4 form-group">
+            <input class="form-control" id="pass" name="password" placeholder="Senha" type="password" required>
+          </div>
 
-    <div class="item">
-      <img src="./images/din.jpg" alt="Los Angeles">
-      <div class="carousel-caption">
-        <h1>Fique de olho no seu dinheiro</h1>
-        <h3>Veja as melhores oportunidades para aplicar o seu dinheiro</h3>
-      </div> 
-    </div>
-  </div>
-
-  <!-- Left and right controls -->
-  <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
+        </div>
+        <div class="row">
+          <div class="col-sm-4 form-group text-align:center"><a href="cadastr-usuario.php">Cadastrar usuário</a></div>
+          <div class="col-sm-4 form-group">
+            <button class="btn pull-right align:right" type="submit">Entrar</button>
+          </div>
+        </div>
+      </form>
+          <div class="row">
+            <?php
+              if ($error!=""){
+                echo '<div class="col-sm-8 form-group alert alert-danger">';
+                echo $error;
+                echo '</div>';
+              }
+            ?>
+          </div>
+      
+		</div>
+	</div>
 </div>
+</body>
+
+
 
 <!-- Footer -->
-<!-- Depois trocar pela chamada da função que mostra o footer --> 
 <footer class="container-fluid">
     <div class="container">
         <div class="media-container-row content text-white">
@@ -164,6 +201,4 @@
   <p>© Copyright 2018 Blablabla - All Rights Reserved</p>
 </footer>
 
-  
-</body>
 </html>
