edb6ab9aaf1949132d1b6006883e81e4709a5a86
Diego Andrade de Mendonça <diego182@gmail.com>
2018-11-06 00:53:42 -0200

    Criação do frontcontroller

--- public/index.php
+++ public/index.php
@@ -2,3 +2,12 @@
 /**
  * System front controller.
  */
+require_once '../vendor/autoload.php';
+require_once '../config/config.php';
+
+use App\AppLoader;
+
+session_start();
+
+$app = new AppLoader();
+$app->run();
