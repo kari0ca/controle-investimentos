27462dd0725b9b47ec448b053f0679fad8a6789a
Diego Andrade de Mendonça <diego182@gmail.com>
2018-11-06 01:48:34 -0200

    Centralizado CSS no arquivo de CSS e não em cada arquivo

--- src/Controllers/cadastr-usuario.php
+++ src/Controllers/cadastr-usuario.php
@@ -1,127 +1,94 @@
 <!DOCTYPE html>
 <?php
-   include("config.php");
-   session_start();
-   
-   $error="";
-   if($_SERVER["REQUEST_METHOD"] == "POST") {
-      $myusername = mysqli_real_escape_string($db,$_POST['nome']);
-      $mylogin = mysqli_real_escape_string($db,$_POST['username']);
-      $myemail = mysqli_real_escape_string($db,$_POST['email']);
-      $mypassword1 = mysqli_real_escape_string($db,$_POST['password1']);
-      $mypassword2 = mysqli_real_escape_string($db,$_POST['password2']); 
-      $mylembr = mysqli_real_escape_string($db,$_POST['lembrete']); 
-      
-      // Procura por usuarios com o mesmo login
-      $sql = "SELECT iduser FROM investdb.user WHERE login = '$mylogin'";
-      $result = mysqli_query($db,$sql);
-      $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-      $active = $row['active'];
-      
-      $count = mysqli_num_rows($result);
-      //echo "<br> count=".$count;
-      if($count >= 1) {
-        $error="Já existe um usuário com este nome, ";
-        //echo "<br> Usuário existente";
-      }
-      if($mypassword1!=$mypassword2) {
-        $error.="A confirmação da senha não é igual, ";
-      }
-      //remover 2 ultimos caracteres da string de erro
-      
-      
-      
-      //echo "<br> antes de buscar o maior id";
-      // Obtem o maior id_user
-      $sql = "SELECT max(iduser) as iduser FROM investdb.user";
-      $result = mysqli_query($db,$sql) or die(mysql_error());
-      $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-      $iduser = $row[iduser];
-      $param_password = password_hash($mypassword1, PASSWORD_DEFAULT);
-      //echo "<br> Maior ID=".$iduser;
-      $iduser = $iduser + 1;
-      //echo "<br> Novo ID=".$iduser;
-      
-      //echo "<br>dados de validação: Nome:".$myusername." Login:".$mylogin." Email:".$myemail." Password:".$param_password." Lembrete:".$mylembr;
-      $sql_insert = "INSERT INTO investdb.user values('".$iduser."','".$myusername."','".$mylogin."','".$param_password."','".$mylembr."','".$myemail."')";
-      
-      if (mysqli_query($db, $sql_insert)) {
-        //echo "New record created successfully";
-      } else {
-        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-      }
+include("config.php");
+session_start();
 
-   }
+$error = "";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $myusername = mysqli_real_escape_string($db, $_POST['nome']);
+    $mylogin = mysqli_real_escape_string($db, $_POST['username']);
+    $myemail = mysqli_real_escape_string($db, $_POST['email']);
+    $mypassword1 = mysqli_real_escape_string($db, $_POST['password1']);
+    $mypassword2 = mysqli_real_escape_string($db, $_POST['password2']);
+    $mylembr = mysqli_real_escape_string($db, $_POST['lembrete']);
+
+    // Procura por usuarios com o mesmo login
+    $sql = "SELECT iduser FROM investdb.user WHERE login = '$mylogin'";
+    $result = mysqli_query($db, $sql);
+    $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+    $active = $row['active'];
+
+    $count = mysqli_num_rows($result);
+    //echo "<br> count=".$count;
+    if ($count >= 1) {
+        $error = "Já existe um usuário com este nome, ";
+        //echo "<br> Usuário existente";
+    }
+    if ($mypassword1 != $mypassword2) {
+        $error .= "A confirmação da senha não é igual, ";
+    }
+    //remover 2 ultimos caracteres da string de erro
+
+
+    //echo "<br> antes de buscar o maior id";
+    // Obtem o maior id_user
+    $sql = "SELECT max(iduser) as iduser FROM investdb.user";
+    $result = mysqli_query($db, $sql) or die(mysql_error());
+    $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+    $iduser = $row[iduser];
+    $param_password = password_hash($mypassword1, PASSWORD_DEFAULT);
+    //echo "<br> Maior ID=".$iduser;
+    $iduser = $iduser + 1;
+    //echo "<br> Novo ID=".$iduser;
+
+    //echo "<br>dados de validação: Nome:".$myusername." Login:".$mylogin." Email:".$myemail." Password:".$param_password." Lembrete:".$mylembr;
+    $sql_insert = "INSERT INTO investdb.user values('" . $iduser . "','" . $myusername . "','" . $mylogin . "','" . $param_password . "','" . $mylembr . "','" . $myemail . "')";
+
+    if (mysqli_query($db, $sql_insert)) {
+        //echo "New record created successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+
+}
 ?>
 
 <html lang="en">
 <head>
-  <title>Controle de investimentos</title>
-  <meta http-equiv="Content-Type" content="text/html;charset=iso-8859-1" />
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-  <style>
-    /* Remove the navbar's default margin-bottom and rounded borders */ 
-    .navbar {
-      margin-bottom: 0;
-      border-radius: 0;
-    }
-    
-    /* Set height of the grid so .sidenav can be 100% (adjust as needed) */
-    .row.content {height: 450px}
-    
-    /* Set gray background color and 100% height */
-    .sidenav {
-      padding-top: 20px;
-      background-color: #f1f1f1;
-      height: 100%;
-    }
-    
-    /* Set black background color, white text and some padding */
-    footer {
-      background-color: #555;
-      color: white;
-      padding: 15px;
-    }
-    
-    /* On small screens, set height to 'auto' for sidenav and grid */
-    @media screen and (max-width: 767px) {
-      .sidenav {
-        height: auto;
-        padding: 15px;
-      }
-      .row.content {height:auto;} 
-    }
-  </style>
+    <title>Controle de investimentos</title>
+    <meta http-equiv="Content-Type" content="text/html;charset=iso-8859-1"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="/css/style.css">
 </head>
 <body>
 
 <!-- Barra de navegação -->
 <nav class="navbar navbar-inverse">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>                        
-      </button>
-      <a class="navbar-brand" href="#">Logo</a>
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Logo</a>
+        </div>
+        <div class="collapse navbar-collapse" id="myNavbar">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="./index.php">Início</a></li>
+                <li><a href="./sobre.php">Sobre</a></li>
+                <li><a href="./contato.php">Contato</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="./login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+            </ul>
+        </div>
     </div>
-    <div class="collapse navbar-collapse" id="myNavbar">
-      <ul class="nav navbar-nav">
-        <li class="active"><a href="./index.php">Início</a></li>
-        <li><a href="./sobre.php">Sobre</a></li>
-        <li><a href="./contato.php">Contato</a></li>
-      </ul>
-      <ul class="nav navbar-nav navbar-right">
-        <li><a href="./login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-      </ul>
-    </div>
-  </div>
 </nav>
-    
+
 
 <!-- Conteúdo -->
 <p></p>
@@ -130,59 +97,61 @@
 
 
 <div class="container">
-	<div class="row justify-content-center">
-		<div class="col-xs-12">
-			<form action = "" method = "post">
-        <div class="row">
-          <div class="col-xs-4 form-group">
-            <input class="form-control" id="nome" name="nome" placeholder="Nome" type="text" required>
-          </div>
-          <div class="col-xs-4 form-group">
-            <input class="form-control" id="login" name="username" placeholder="Login" type="text" required>
-          </div>
+    <div class="row justify-content-center">
+        <div class="col-xs-12">
+            <form action="" method="post">
+                <div class="row">
+                    <div class="col-xs-4 form-group">
+                        <input class="form-control" id="nome" name="nome" placeholder="Nome" type="text" required>
+                    </div>
+                    <div class="col-xs-4 form-group">
+                        <input class="form-control" id="login" name="username" placeholder="Login" type="text" required>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-xs-8 form-group">
+                        <input class="form-control" id="email" name="email" placeholder="E-mail" type="email" required>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-xs-4 form-group">
+                        <input class="form-control" id="pass1" name="password1" placeholder="Senha" type="password"
+                               required>
+                    </div>
+                    <div class="col-xs-4 form-group">
+                        <input class="form-control" id="pass2" name="password2" placeholder="Repita a Senha"
+                               type="password" required>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-xs-4 form-group">
+                        <input class="form-control" id="lembrete" name="lembrete" placeholder="Lembrete da senha"
+                               type="text" required>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-xs-4 form-group">
+                        <button class="btn btn-danger pull-right align:right" type="reset">Cancelar</button>
+                    </div>
+                    <div class="col-xs-4 form-group">
+                        <button class="btn btn-default pull-right align:left " type="submit">Cadastrar</button>
+                    </div>
+                </div>
+            </form>
+            <div class="row">
+                <?php
+                if ($error != "") {
+                    echo '<div class="col-xs-8 form-group alert alert-danger">';
+                    echo $error;
+                    echo '</div>';
+                }
+                ?>
+            </div>
+
         </div>
-        <div class="row">
-          <div class="col-xs-8 form-group">
-            <input class="form-control" id="email" name="email" placeholder="E-mail" type="email" required>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-xs-4 form-group">
-            <input class="form-control" id="pass1" name="password1" placeholder="Senha" type="password" required>
-          </div>
-          <div class="col-xs-4 form-group">
-            <input class="form-control" id="pass2" name="password2" placeholder="Repita a Senha" type="password" required>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-xs-4 form-group">
-            <input class="form-control" id="lembrete" name="lembrete" placeholder="Lembrete da senha" type="text" required>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-xs-4 form-group">
-            <button class="btn btn-danger pull-right align:right" type="reset">Cancelar</button>
-          </div>
-          <div class="col-xs-4 form-group">
-            <button class="btn btn-default pull-right align:left " type="submit">Cadastrar</button>
-          </div>
-        </div>
-      </form>
-          <div class="row">
-            <?php
-              if ($error!=""){
-                echo '<div class="col-xs-8 form-group alert alert-danger">';
-                echo $error;
-                echo '</div>';
-              }
-            ?>
-          </div>
-      
-		</div>
-	</div>
+    </div>
 </div>
 </body>
-
 
 
 <!-- Footer -->
@@ -221,13 +190,14 @@
                 </h5>
                 <p class="mbr-text">
                     <a class="text-primary" href="https://xxxxxxxxxx.com">Website builder</a>
-                    <br><a class="text-primary" href="https://xxxxxxxxxx.commobirise-free-win.zip">Download for Windows</a>
+                    <br><a class="text-primary" href="https://xxxxxxxxxx.commobirise-free-win.zip">Download for
+                        Windows</a>
                     <br><a class="text-primary" href="https://xxxxxxxxxx.commobirise-free-mac.zip">Download for Mac</a>
                 </p>
             </div>
         </div>
-  
-  <p>© Copyright 2018 Blablabla - All Rights Reserved</p>
+
+        <p>© Copyright 2018 Blablabla - All Rights Reserved</p>
 </footer>
 
 </html>
--- src/Controllers/login.php
+++ src/Controllers/login.php
@@ -1,155 +1,123 @@
 <!DOCTYPE html>
 <?php
-   include("config.php");
-   session_start();
-   
-   $error="";
-   if($_SERVER["REQUEST_METHOD"] == "POST") {
-      // username and password sent from form 
-      
-      $myusername = mysqli_real_escape_string($db,$_POST['username']);
-      $mypassword = mysqli_real_escape_string($db,$_POST['password']);
-      
-      $param_password = password_hash($mypassword, PASSWORD_DEFAULT);
-      
-      $sql = "SELECT iduser, pass FROM investdb.user WHERE login = '$myusername'";
-      $result = mysqli_query($db,$sql);
-      $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-      $senha_hash = $row[pass];
-      $active = $row['active'];
-      $iduser = $row[iduser];
-      
-      //compara senha com hash
-      if (password_verify ($mypassword, $senha_hash)){
+include("config.php");
+session_start();
+
+$error = "";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // username and password sent from form
+
+    $myusername = mysqli_real_escape_string($db, $_POST['username']);
+    $mypassword = mysqli_real_escape_string($db, $_POST['password']);
+
+    $param_password = password_hash($mypassword, PASSWORD_DEFAULT);
+
+    $sql = "SELECT iduser, pass FROM investdb.user WHERE login = '$myusername'";
+    $result = mysqli_query($db, $sql);
+    $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+    $senha_hash = $row[pass];
+    $active = $row['active'];
+    $iduser = $row[iduser];
+
+    //compara senha com hash
+    if (password_verify($mypassword, $senha_hash)) {
         //echo "<br>Senha verificada com sucesso";
-      } else {
+    } else {
         //echo "<br>Senha não verificada";
-      }
-      
-      $count = mysqli_num_rows($result);
-      
-      // If result matched $myusername and $mypassword, table row must be 1 row
-		
-      if($count == 1) {
-         $_SESSION["login_user"] = $myusername;
-         $_SESSION["iduser"] = $iduser;
-         header("location:carteira.php"); die('Não ignore meu cabeçalho...');
-      }else {
-         $error = "Login ou Senha invalidos";
-      }
-   }
+    }
+
+    $count = mysqli_num_rows($result);
+
+    // If result matched $myusername and $mypassword, table row must be 1 row
+
+    if ($count == 1) {
+        $_SESSION["login_user"] = $myusername;
+        $_SESSION["iduser"] = $iduser;
+        header("location:carteira.php");
+        die('Não ignore meu cabeçalho...');
+    } else {
+        $error = "Login ou Senha invalidos";
+    }
+}
 ?>
 
 <html lang="en">
 <head>
-  <title>Controle de investimentos</title>
-  <meta http-equiv="Content-Type" content="text/html;charset=iso-8859-1" />
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-  <style>
-    /* Remove the navbar's default margin-bottom and rounded borders */ 
-    .navbar {
-      margin-bottom: 0;
-      border-radius: 0;
-    }
-    
-    /* Set height of the grid so .sidenav can be 100% (adjust as needed) */
-    .row.content {height: 450px}
-    
-    /* Set gray background color and 100% height */
-    .sidenav {
-      padding-top: 20px;
-      background-color: #f1f1f1;
-      height: 100%;
-    }
-    
-    /* Set black background color, white text and some padding */
-    footer {
-      background-color: #555;
-      color: white;
-      padding: 15px;
-    }
-    
-    /* On small screens, set height to 'auto' for sidenav and grid */
-    @media screen and (max-width: 767px) {
-      .sidenav {
-        height: auto;
-        padding: 15px;
-      }
-      .row.content {height:auto;} 
-    }
-  </style>
+    <title>Controle de investimentos</title>
+    <meta http-equiv="Content-Type" content="text/html;charset=iso-8859-1"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="/css/style.css">
 </head>
 <body>
 
 <!-- Barra de navegação -->
 <nav class="navbar navbar-inverse">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>                        
-      </button>
-      <a class="navbar-brand" href="#">Logo</a>
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Logo</a>
+        </div>
+        <div class="collapse navbar-collapse" id="myNavbar">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="./index.php">Início</a></li>
+                <li><a href="./sobre.php">Sobre</a></li>
+                <li><a href="./contato.php">Contato</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="./login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+            </ul>
+        </div>
     </div>
-    <div class="collapse navbar-collapse" id="myNavbar">
-      <ul class="nav navbar-nav">
-        <li class="active"><a href="./index.php">Início</a></li>
-        <li><a href="./sobre.php">Sobre</a></li>
-        <li><a href="./contato.php">Contato</a></li>
-      </ul>
-      <ul class="nav navbar-nav navbar-right">
-        <li><a href="./login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-      </ul>
-    </div>
-  </div>
 </nav>
-    
 
 <!-- Conteúdo -->
 <p></p>
 <span></span>
 <span></span>
 
-
 <div class="container">
-	<div class="row justify-content-center">
-		<div class="col-xs-12">
-			<form action = "" method = "post">
-        <div class="row">
-          <div class="col-xs-4 form-group">
-            <input class="form-control" id="login" name="username" placeholder="Login" type="text" required>
-          </div>
-          <div class="col-xs-4 form-group">
-            <input class="form-control" id="pass" name="password" placeholder="Senha" type="password" required>
-          </div>
+    <div class="row justify-content-center">
+        <div class="col-xs-12">
+            <form action="" method="post">
+                <div class="row">
+                    <div class="col-xs-4 form-group">
+                        <input class="form-control" id="login" name="username" placeholder="Login" type="text" required>
+                    </div>
+                    <div class="col-xs-4 form-group">
+                        <input class="form-control" id="pass" name="password" placeholder="Senha" type="password"
+                               required>
+                    </div>
+
+                </div>
+                <div class="row">
+                    <div class="col-xs-4 form-group text-align:center"><a href="cadastr-usuario.php">Cadastrar
+                            usuário</a></div>
+                    <div class="col-xs-4 form-group">
+                        <button class="btn pull-right align:right" type="submit">Entrar</button>
+                    </div>
+                </div>
+            </form>
+            <div class="row">
+                <?php
+                if ($error != "") {
+                    echo '<div class="col-xs-8 form-group alert alert-danger">';
+                    echo $error;
+                    echo '</div>';
+                }
+                ?>
+            </div>
 
         </div>
-        <div class="row">
-          <div class="col-xs-4 form-group text-align:center"><a href="cadastr-usuario.php">Cadastrar usuário</a></div>
-          <div class="col-xs-4 form-group">
-            <button class="btn pull-right align:right" type="submit">Entrar</button>
-          </div>
-        </div>
-      </form>
-          <div class="row">
-            <?php
-              if ($error!=""){
-                echo '<div class="col-xs-8 form-group alert alert-danger">';
-                echo $error;
-                echo '</div>';
-              }
-            ?>
-          </div>
-      
-		</div>
-	</div>
+    </div>
 </div>
 </body>
-
 
 
 <!-- Footer -->
@@ -188,13 +156,14 @@
                 </h5>
                 <p class="mbr-text">
                     <a class="text-primary" href="https://xxxxxxxxxx.com">Website builder</a>
-                    <br><a class="text-primary" href="https://xxxxxxxxxx.commobirise-free-win.zip">Download for Windows</a>
+                    <br><a class="text-primary" href="https://xxxxxxxxxx.commobirise-free-win.zip">Download for
+                        Windows</a>
                     <br><a class="text-primary" href="https://xxxxxxxxxx.commobirise-free-mac.zip">Download for Mac</a>
                 </p>
             </div>
         </div>
-  
-  <p>© Copyright 2018 Blablabla - All Rights Reserved</p>
+
+        <p>© Copyright 2018 Blablabla - All Rights Reserved</p>
 </footer>
 
 </html>
