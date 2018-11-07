6246cd13a2f5e4a656f5660f4d39372db7711d57
Diego Andrade de Mendonça <diego182@gmail.com>
2018-11-07 01:22:52 -0200

    Remoção da classe de entity e uso de única classe utilizando p padrão active record

--- src/Models/Entities/Type.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App\Models\Entities;
-
-class Type
-{
-
-}
--- src/Models/Type.php
+++ src/Models/Type.php
@@ -4,7 +4,7 @@ namespace App\Models\Tables;
 
 use App\Models\Entities\Type;
 
-class Types
+class Type
 {
     public function add(Type $type)
     {
