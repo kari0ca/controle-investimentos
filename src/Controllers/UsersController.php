d69a3fd14a288083659a44b3b924e76b3701f474
Diego Andrade de Mendonça <diego182@gmail.com>
2018-11-08 01:10:53 -0200

    Conversão do cadastro de usuários para PDO

--- src/Controllers/UsersController.php
+++ src/Controllers/UsersController.php
@@ -19,7 +19,7 @@ class UsersController extends Controller
         if (isset($_SESSION['user'])) {
             return header('Location: /?controller=App\Controllers\Wallet');
         }
-        
+
         $error = '';
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             try {
@@ -106,203 +106,116 @@ class UsersController extends Controller
 
     public function add()
     {
-        echo '<!DOCTYPE html>';
-
         $error = "";
-        if ($_SERVER["REQUEST_METHOD"] == "POST") {
-            $myusername = mysqli_real_escape_string($db, $_POST['nome']);
-            $mylogin = mysqli_real_escape_string($db, $_POST['username']);
-            $myemail = mysqli_real_escape_string($db, $_POST['email']);
-            $mypassword1 = mysqli_real_escape_string($db, $_POST['password1']);
-            $mypassword2 = mysqli_real_escape_string($db, $_POST['password2']);
-            $mylembr = mysqli_real_escape_string($db, $_POST['lembrete']);
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            try {
+                Transaction::open();
+                $connection = Transaction::get();
 
-            // Procura por usuarios com o mesmo login
-            $sql = "SELECT iduser FROM investdb.user WHERE login = '$mylogin'";
-            $result = mysqli_query($db, $sql);
-            $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
-            $active = $row['active'];
+                $statement = $connection->prepare('SELECT count(*) FROM `user` WHERE `login` = :login');
+                $statement->bindParam(':login', $_POST['username'], \PDO::PARAM_STR);
+                $statement->execute();
+                $count = $statement->fetchColumn();
 
-            $count = mysqli_num_rows($result);
-            //echo "<br> count=".$count;
-            if ($count >= 1) {
-                $error = "Já existe um usuário com este nome, ";
-                //echo "<br> Usuário existente";
+                if ((int)$count > 0) {
+                    $error = 'Já existe um usuário com este nome';
+                }
+
+                if ($_POST['password1'] != $_POST['password2']) {
+                    $error .= 'A confirmação da senha não é igual';
+                }
+
+                $statement = $connection->prepare('SELECT max(`iduser`)  FROM `user`');
+                $statement->execute();
+                $lastId = $statement->fetchColumn();
+
+                $passwordHash = password_hash($_POST['password1'], PASSWORD_DEFAULT);
+                $idUser = $lastId + 1;
+
+                $statement = $connection->prepare('INSERT INTO user (`iduser`, `nome`, `login`, `pass`, `aux_senha`, `email`) VALUES (:iduser, :nome, :login, :pass, :aux_senha, :email)');
+                $statement->bindParam(':iduser', $idUser, \PDO::PARAM_INT);
+                $statement->bindParam(':nome', $_POST['nome'], \PDO::PARAM_STR);
+                $statement->bindParam(':login', $_POST['username'], \PDO::PARAM_STR);
+                $statement->bindParam(':pass', $passwordHash, \PDO::PARAM_STR);
+                $statement->bindParam(':aux_senha', $_POST['lembrete'], \PDO::PARAM_STR);
+                $statement->bindParam(':email', $_POST['email'], \PDO::PARAM_STR);
+                $statement->execute();
+
+                Transaction::close();
+            } catch (\Exception $exception) {
+                Transaction::rollback();
+                throw $exception;
             }
-            if ($mypassword1 != $mypassword2) {
-                $error .= "A confirmação da senha não é igual, ";
-            }
-            //remover 2 ultimos caracteres da string de erro
-
-
-            //echo "<br> antes de buscar o maior id";
-            // Obtem o maior id_user
-            $sql = "SELECT max(iduser) as iduser FROM investdb.user";
-            $result = mysqli_query($db, $sql) or die(mysql_error());
-            $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
-            $iduser = $row[iduser];
-            $param_password = password_hash($mypassword1, PASSWORD_DEFAULT);
-            //echo "<br> Maior ID=".$iduser;
-            $iduser = $iduser + 1;
-            //echo "<br> Novo ID=".$iduser;
-
-            //echo "<br>dados de validação: Nome:".$myusername." Login:".$mylogin." Email:".$myemail." Password:".$param_password." Lembrete:".$mylembr;
-            $sql_insert = "INSERT INTO investdb.user values('" . $iduser . "','" . $myusername . "','" . $mylogin . "','" . $param_password . "','" . $mylembr . "','" . $myemail . "')";
-
-            if (mysqli_query($db, $sql_insert)) {
-                //echo "New record created successfully";
-            } else {
-                echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-            }
-
         }
-        echo '
-        <html lang="en">
-        <head>
-            <title>Controle de investimentos</title>
-            <meta http-equiv="Content-Type" content="text/html;charset=iso-8859-1"/>
-            <meta name="viewport" content="width=device-width, initial-scale=1">
-            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-            <link rel="stylesheet" href="/css/style.css">
-        </head>
-        <body>
-        
-        <!-- Barra de navegação -->
-        <nav class="navbar navbar-inverse">
-            <div class="container-fluid">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a class="navbar-brand" href="#">Logo</a>
-                </div>
-                <div class="collapse navbar-collapse" id="myNavbar">
-                    <ul class="nav navbar-nav">
-                        <li class="active"><a href="./index.php">Início</a></li>
-                        <li><a href="./sobre.php">Sobre</a></li>
-                        <li><a href="./contato.php">Contato</a></li>
-                    </ul>
-                    <ul class="nav navbar-nav navbar-right">
-                        <li><a href="./login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
+
+        require_once 'header.php';
+        echo '<body>';
+        echo '<!--Barra de navegação-->';
+
+        require_once 'menu.php';
 
 
-        <!-- Conteúdo -->
-        <p></p>
-        <span></span>
-        <span></span>
+        echo '<!--Conteúdo -->
+        <p ></p >
+        <span ></span >
+        <span ></span >
         
 
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-xs-12">
-                    <form action="" method="post">
-                        <div class="row">
-                            <div class="col-xs-4 form-group">
-                                <input class="form-control" id="nome" name="nome" placeholder="Nome" type="text" required>
-                            </div>
-                            <div class="col-xs-4 form-group">
-                                <input class="form-control" id="login" name="username" placeholder="Login" type="text" required>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-xs-8 form-group">
-                                <input class="form-control" id="email" name="email" placeholder="E-mail" type="email" required>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-xs-4 form-group">
-                                <input class="form-control" id="pass1" name="password1" placeholder="Senha" type="password"
-                                       required>
-                            </div>
-                            <div class="col-xs-4 form-group">
-                                <input class="form-control" id="pass2" name="password2" placeholder="Repita a Senha"
-                                       type="password" required>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-xs-4 form-group">
-                                <input class="form-control" id="lembrete" name="lembrete" placeholder="Lembrete da senha"
-                                       type="text" required>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-xs-4 form-group">
-                                <button class="btn btn-danger pull-right align:right" type="reset">Cancelar</button>
-                            </div>
-                            <div class="col-xs-4 form-group">
-                                <button class="btn btn-default pull-right align:left " type="submit">Cadastrar</button>
-                            </div>
-                        </div>
-                    </form>
-                    <div class="row">';
+        <div class="container" >
+            <div class="row justify-content-center" >
+                <div class="col-xs-12" >
+                    <form action = "" method = "post" >
+                        <div class="row" >
+                            <div class="col-xs-4 form-group" >
+                                <input class="form-control" id = "nome" name = "nome" placeholder = "Nome" type = "text" required >
+                            </div >
+                            <div class="col-xs-4 form-group" >
+                                <input class="form-control" id = "login" name = "username" placeholder = "Login" type = "text" required >
+                            </div >
+                        </div >
+                        <div class="row" >
+                            <div class="col-xs-8 form-group" >
+                                <input class="form-control" id = "email" name = "email" placeholder = "E-mail" type = "email" required >
+                            </div >
+                        </div >
+                        <div class="row" >
+                            <div class="col-xs-4 form-group" >
+                                <input class="form-control" id = "pass1" name = "password1" placeholder = "Senha" type = "password"
+                                       required >
+                            </div >
+                            <div class="col-xs-4 form-group" >
+                                <input class="form-control" id = "pass2" name = "password2" placeholder = "Repita a Senha"
+                                       type = "password" required >
+                            </div >
+                        </div >
+                        <div class="row" >
+                            <div class="col-xs-4 form-group" >
+                                <input class="form-control" id = "lembrete" name = "lembrete" placeholder = "Lembrete da senha"
+                                       type = "text" required >
+                            </div >
+                        </div >
+                        <div class="row" >
+                            <div class="col-xs-4 form-group" >
+                                <button class="btn btn-danger pull-right align:right" type = "reset" > Cancelar</button >
+                            </div >
+                            <div class="col-xs-4 form-group" >
+                                <button class="btn btn-default pull-right align:left " type = "submit" > Cadastrar</button >
+                            </div >
+                        </div >
+                    </form >
+                    <div class="row" > ';
         if ($error != "") {
-            echo '<div class="col-xs-8 form-group alert alert-danger">';
+            echo '<div class="col-xs-8 form-group alert alert-danger" > ';
             echo $error;
-            echo '</div>';
+            echo '</div > ';
         }
 
-        echo '</div>
-
-                    </div>
-                </div>
-            </div>
-            </body>
-
-
-            <!-- Footer -->
-            <footer class="container-fluid">
-                <div class="container">
-                    <div class="media-container-row content text-white">
-                        <div class="col-12 col-xs-3">
-                            <div class="media-wrap">
-                                <a href="https://xxxxxxxxxx.com">
-                                    <img src="assets/images/logo24.png" alt="Mobirise">
-                                </a>
-                            </div>
-                        </div>
-                        <div class="col-12 col-xs-3 mbr-fonts-style display-7">
-                            <h5 class="pb-3">
-                                Address
-                            </h5>
-                            <p class="mbr-text">
-                                1234 Street Name
-                                <br>City, AA 99999
-                            </p>
-                        </div>
-                        <div class="col-12 col-xs-3 mbr-fonts-style display-7">
-                            <h5 class="pb-3">
-                                Contacts
-                            </h5>
-                            <p class="mbr-text">
-                                Email: support@mobirise.com
-                                <br>Phone: +1 (0) 000 0000 001
-                                <br>Fax: +1 (0) 000 0000 002
-                            </p>
-                        </div>
-                        <div class="col-12 col-xs-3 mbr-fonts-style display-7">
-                            <h5 class="pb-3">
-                                Links
-                            </h5>
-                            <p class="mbr-text">
-                                <a class="text-primary" href="https://xxxxxxxxxx.com">Website builder</a>
-                                <br><a class="text-primary" href="https://xxxxxxxxxx.commobirise-free-win.zip">Download for
-                                    Windows</a>
-                                <br><a class="text-primary" href="https://xxxxxxxxxx.commobirise-free-mac.zip">Download for Mac</a>
-                            </p>
-                        </div>
-                    </div>
-            
-                    <p>© Copyright 2018 Blablabla - All Rights Reserved</p>
-            </footer>
-            
-            </html>';
+        echo '</div>';
+        echo '</div>';
+        echo '</div>';
+        echo '</div>';
+        require 'footer.php';
+        echo '</body>';
+        echo '</html>';
     }
 }
