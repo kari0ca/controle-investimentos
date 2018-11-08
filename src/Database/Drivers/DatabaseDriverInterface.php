9c2518c1e3893c9b3f0518f20f3fbb34b073c0d7
Diego Andrade de Mendonça <diego182@gmail.com>
2018-11-07 23:18:52 -0200

    Improved connection documentation for the interface

--- src/Database/Drivers/DatabaseDriverInterface.php
+++ src/Database/Drivers/DatabaseDriverInterface.php
@@ -12,6 +12,8 @@ namespace App\Database\Drivers;
 interface DatabaseDriverInterface
 {
     /**
+     * Return a new PDO instance for a givem instance
+     *
      * @return \PDO
      */
     public function getConnection(): \PDO;
